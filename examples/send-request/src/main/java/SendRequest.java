7e6db0bcbab49bce7c1a443dff65bba1e348cf55
prebala1 <pranav.rebala@opsmx.io>
2022-07-14 18:42:14 -0700

    creates nodes in database but not relationships yet, also has incomplete schema

--- examples/send-request/src/main/java/SendRequest.java
+++ examples/send-request/src/main/java/SendRequest.java
@@ -6,16 +6,104 @@ import org.json.JSONStringer;
 import java.io.*;
 import java.rmi.server.RemoteRef;
 import java.util.Objects;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 
 public class SendRequest {
     public static void main(String[] args) {
-        getCastByMovie("Top Gun");
-        //getMoviesByPerson("Tom Cruise");
-        //getMoviesBetweenDates(2000, 2022);
+        addData();
     }
 
-    static String path = "C:/Users/rebal/movies/movies-database/neo4j-graphql-java-master/JSONfiles/output.txt";
+    static String path = "C:/Users/rebal/pipeline/pipelinedb/JSONfiles/pipeline.json";
+
+    public static void addData() {
+        JSONObject jo2 = readJSON();
+        addPipeline(jo2);
+        addStages(jo2);
+        addPrevStages(jo2);
+    }
+
+    public static void addPipeline(JSONObject jo2) {
+        String id = (String) jo2.get("id");
+        String mutation = "mutation {\n" +
+                "  createPipeline(input: {id: \""+id+"\"}) {\n" +
+                "    id\n" +
+                "  }\n" +
+                "}";
+        JSONObject jo = new JSONObject();
+        jo.put("query", mutation);
+        try {
+            whenSendPostRequest_thenCorrect(jo);
+        }
+        catch (IOException ignored) {}
+    }
+
+    public static void addStages(JSONObject jo2) {
+        JSONArray stages = jo2.getJSONArray("stages");
+        for (int i = 0; i < stages.length(); i++) {
+            String id = (String) stages.getJSONObject(i).get("refId");
+            String mutation = "mutation {\n" +
+                    "  createStages(input: {refId: \"" + id + "\"}) {\n" +
+                    "    refId\n" +
+                    "  }\n" +
+                    "}";
+            JSONObject jo = new JSONObject();
+            jo.put("query", mutation);
+            try {
+                whenSendPostRequest_thenCorrect(jo);
+            } catch (IOException ignored) {
+            }
+        }
+    }
+
+    public static void addPrevStages(JSONObject jo2) {
+        JSONArray stages = jo2.getJSONArray("stages");
+        for (int i = 0; i < stages.length(); i++) {
+            String id = (String) stages.getJSONObject(i).get("refId");
+            System.out.println(stages.getJSONObject(i).get("requisiteStageRefIds"));
+            JSONArray temp = (JSONArray) stages.getJSONObject(i).get("requisiteStageRefIds");
+            /*String[] prevs = (String[]) stages.getJSONObject(i).get("requisiteStageRefIds");
+            for (int j = 0; j < prevs.length; j++) {
+                String mutation = "mutation {\n" +
+                        "  createStageTimeline(input: {s1: \""+id+"\", s2: \""+prevs[j]+"\"}) {\n" +
+                        "    refId\n" +
+                        "  }\n" +
+                        "}";
+                System.out.println(mutation);
+                JSONObject jo = new JSONObject();
+                jo.put("query", mutation);
+                try {
+                    whenSendPostRequest_thenCorrect(jo);
+                } catch (IOException ignored) {
+                }
+            }*/
+        }
+    }
+
+    public static JSONObject readJSON() {
+        JSONObject jo = null;
+        try {
+            File myObj = new File(path);
+            Scanner myReader = new Scanner(myObj);
+            String allJson = "";
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                //System.out.println(data);
+                allJson += data;
+            }
+            //allJson += "]}";
+            //System.out.println(allJson);
+            jo = new JSONObject(allJson);
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        //System.out.println(jo);
+        return jo;
+    }
 
     public static void sendAnyQuery(String query) {
         JSONObject jo = new JSONObject();
@@ -24,85 +112,6 @@ public class SendRequest {
         try (PrintWriter out = new PrintWriter(new FileWriter(path))){
             JSONObject results = whenSendPostRequest_thenCorrect(jo);
             //System.out.println(results);
-            //results.write(out);
-            out.write(results.toString(4));
-        }
-        catch (IOException ignored) {
-
-        }
-    }
-
-    public static void getMoviesBetweenDates(int start, int end) {
-        String query = "query { \n" +
-                "  movies (where : {released_gte : "+start+", released_lte : "+end+"}){\n" +
-                "    title\n" +
-                "  }\n" +
-                "}";
-        JSONObject jo = new JSONObject();
-        jo.put("query", query);
-        //System.out.println(jo);
-        try (PrintWriter out = new PrintWriter(new FileWriter(path))){
-            JSONObject results = whenSendPostRequest_thenCorrect(jo);
-            //System.out.println(results);
-            JSONArray data = results.getJSONObject("data").getJSONArray("movies");
-            for (int i = 0; i < data.length(); i++) {
-                System.out.println(data.getJSONObject(i).get("title"));
-            }
-            //results.write(out);
-            out.write(results.toString(4));
-        }
-        catch (IOException ignored) {
-
-        }
-    }
-
-    public static void getCastByMovie(String title) {
-        String query = "query {\n" +
-                "  movies (where:{title:\""+title+"\"}){\n" +
-                "    cast {\n" +
-                "      name\n" +
-                "    }\n" +
-                "  }\n" +
-                "}";
-        JSONObject jo = new JSONObject();
-        jo.put("query", query);
-        //System.out.println(jo);
-
-        try (PrintWriter out = new PrintWriter(new FileWriter(path))){
-            JSONObject results = whenSendPostRequest_thenCorrect(jo);
-            //System.out.println(results);
-            JSONArray data = results.getJSONObject("data").getJSONArray("movies")
-                    .getJSONObject(0).getJSONArray("cast");
-            for (int i = 0; i < data.length(); i++) {
-                System.out.println(data.getJSONObject(i).get("name"));
-            }
-            out.write(results.toString(4));
-            //results.write(out);
-        }
-        catch (IOException e) {
-            System.out.println("fail");
-        }
-    }
-
-    public static void getMoviesByPerson(String name) {
-        String query = "query {\n" +
-                "  people (where:{name:\""+name+"\"}){\n" +
-                "    history {\n" +
-                "      title\n" +
-                "    }\n" +
-                "  }\n" +
-                "}";
-        JSONObject jo = new JSONObject();
-        jo.put("query", query);
-        //System.out.println(jo);
-        try (PrintWriter out = new PrintWriter(new FileWriter(path))){
-            JSONObject results = whenSendPostRequest_thenCorrect(jo);
-            //System.out.println(results);
-            JSONArray data = results.getJSONObject("data").getJSONArray("people")
-                    .getJSONObject(0).getJSONArray("history");
-            for (int i = 0; i < data.length(); i++) {
-                System.out.println(data.getJSONObject(i).get("title"));
-            }
             //results.write(out);
             out.write(results.toString(4));
         }

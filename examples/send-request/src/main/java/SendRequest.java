3d71232e8cf360964cd1fa00e80ec8a984bd5451
prebala1 <pranav.rebala@opsmx.io>
2022-07-15 18:50:51 -0700

    added trigger

--- examples/send-request/src/main/java/SendRequest.java
+++ examples/send-request/src/main/java/SendRequest.java
@@ -1,5 +1,6 @@
 import okhttp3.*;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONStringer;
 
@@ -21,6 +22,7 @@ public class SendRequest {
     public static void addData() {
         JSONObject jo2 = readJSON();
         addPipeline(jo2);
+        addTrigger(jo2);
         addStages(jo2);
         addPrevStages(jo2);
     }
@@ -33,6 +35,34 @@ public class SendRequest {
                 "  }\n" +
                 "}";
         JSONObject jo = new JSONObject();
+        jo.put("query", mutation);
+        try {
+            whenSendPostRequest_thenCorrect(jo);
+        }
+        catch (IOException ignored) {}
+    }
+
+    public static void addTrigger(JSONObject jo2) {
+        String id = (String) jo2.getJSONObject("trigger").get("executionId");
+        String mutation = "mutation {\n" +
+                "  createTrigger(input: {executionId: \""+id+"\"}) {\n" +
+                "    executionId\n" +
+                "  }\n" +
+                "}";
+        JSONObject jo = new JSONObject();
+        jo.put("query", mutation);
+        try {
+            whenSendPostRequest_thenCorrect(jo);
+        }
+        catch (IOException ignored) {}
+
+        String id2 = (String) jo2.get("id");
+        mutation = "mutation {\n" +
+                "  createPipelineTrigger(input: {s1: \""+id+"\", s2: \""+id2+"\"}) {\n" +
+                "    id\n" +
+                "  }\n" +
+                "}";
+        jo = new JSONObject();
         jo.put("query", mutation);
         try {
             whenSendPostRequest_thenCorrect(jo);
@@ -55,6 +85,7 @@ public class SendRequest {
                 whenSendPostRequest_thenCorrect(jo);
             } catch (IOException ignored) {
             }
+            addOutputs(stages.getJSONObject(i), id);
         }
     }
 
@@ -65,7 +96,7 @@ public class SendRequest {
             JSONArray prevs = (JSONArray) stages.getJSONObject(i).get("requisiteStageRefIds");
             if (prevs.length() == 0) {
                 String mutation = "mutation {\n" +
-                        "  createStagePipeline(input: {s1: \""+jo2.get("id")+"\", s2: \""+id+"\"}) {\n" +
+                        "  createStageTrigger(input: {s1: \""+jo2.get("id")+"\", s2: \""+id+"\"}) {\n" +
                         "    refId\n" +
                         "  }\n" +
                         "}";
@@ -90,6 +121,44 @@ public class SendRequest {
                 }
             }
         }
+    }
+
+    public static void addOutputs(JSONObject jo2, String id) {
+        String jobNumber = "unknown";
+        try {
+            jobNumber = (String) jo2.getJSONObject("outputs").get("JobNumber");
+        } catch (JSONException ignored) {
+
+        }
+        System.out.println(jo2.getJSONObject("outputs").keySet());
+        if (!jobNumber.equals("unknown")) {
+            String mutation = "mutation {\n" +
+                    "  createOutputs(input: {JobNumber: \"" + jobNumber + "\"}) {\n" +
+                    "    JobNumber\n" +
+                    "  }\n" +
+                    "}";
+            JSONObject jo = new JSONObject();
+            jo.put("query", mutation);
+            try {
+                whenSendPostRequest_thenCorrect(jo);
+            } catch (IOException ignored) {
+            }
+            connectOutputs(jobNumber, id);
+        }
+    }
+
+    public static void connectOutputs(String jobNumber, String id) {
+        String mutation = "mutation {\n" +
+                "  createStageOutputs(input: {s1: \""+id+"\", s2: \""+jobNumber+"\"}) {\n" +
+                "    refId\n" +
+                "  }\n" +
+                "}";
+        JSONObject jo = new JSONObject();
+        jo.put("query", mutation);
+        try {
+            whenSendPostRequest_thenCorrect(jo);
+        }
+        catch (IOException ignored) {}
     }
 
     public static JSONObject readJSON() {

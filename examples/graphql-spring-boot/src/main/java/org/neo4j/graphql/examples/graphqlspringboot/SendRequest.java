e65f425672cc8f005a6c73846884885294156408
prebala1 <pranav.rebala@opsmx.io>
2022-07-16 22:16:07 -0700

    added more fields for stages and pipeline

--- examples/graphql-spring-boot/src/main/java/org/neo4j/graphql/examples/graphqlspringboot/SendRequest.java
+++ examples/graphql-spring-boot/src/main/java/org/neo4j/graphql/examples/graphqlspringboot/SendRequest.java
@@ -4,11 +4,11 @@ import okhttp3.*;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.json.JSONStringer;
 
 import java.io.*;
-import java.rmi.server.RemoteRef;
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -30,9 +30,25 @@ public class SendRequest {
     }
 
     public static void addPipeline(JSONObject jo2) {
-        String id = (String) jo2.get("id");
+        Iterator<String> it = jo2.keys();
+        ArrayList<String> lst = new ArrayList<>();
+        while (it.hasNext()) {
+            String k = it.next();
+            if (k.equals("systemNotifications") || k.equals("trigger") || k.equals("initialConfig") || k.equals("stages")
+                    || k.equals("notifications") || k.equals("authentication")) {
+                continue;
+            }
+            else if (k.equals("startTime") || k.equals("endTime") || k.equals("buildTime") || k.equals("canceled")
+                    || k.equals("limitConcurrent") || k.equals("keepWaitingPipelines")) {
+                lst.add(k+": "+jo2.get(k)+"");
+            }
+            else {
+                lst.add(k + ": \"" + jo2.get(k) + "\"");
+            }
+        }
+        String fields = String.join(", ", lst);
         String mutation = "mutation {\n" +
-                "  createPipeline(input: {id: \""+id+"\"}) {\n" +
+                "  createPipeline(input: {"+fields+"}) {\n" +
                 "    id\n" +
                 "  }\n" +
                 "}";
@@ -76,8 +92,23 @@ public class SendRequest {
         JSONArray stages = jo2.getJSONArray("stages");
         for (int i = 0; i < stages.length(); i++) {
             String id = (String) stages.getJSONObject(i).get("refId");
+            Iterator<String> it = stages.getJSONObject(i).keys();
+            ArrayList<String> lst = new ArrayList<>();
+            while (it.hasNext()) {
+                String k = it.next();
+                if (k.equals("outputs") || k.equals("tasks") || k.equals("context") || k.equals("requisiteStageRefIds")) {
+                    continue;
+                }
+                else if (k.equals("startTime") || k.equals("endTime")) {
+                    lst.add(k+": "+stages.getJSONObject(i).get(k)+"");
+                }
+                else {
+                    lst.add(k + ": \"" + stages.getJSONObject(i).get(k) + "\"");
+                }
+            }
+            String fields = String.join(", ", lst);
             String mutation = "mutation {\n" +
-                    "  createStages(input: {refId: \"" + id + "\"}) {\n" +
+                    "  createStages(input: {" + fields + "}) {\n" +
                     "    refId\n" +
                     "  }\n" +
                     "}";
@@ -132,7 +163,7 @@ public class SendRequest {
         } catch (JSONException ignored) {
 
         }
-        System.out.println(jo2.getJSONObject("outputs").keySet());
+        //System.out.println(jo2.getJSONObject("outputs").keySet());
         if (!jobNumber.equals("unknown")) {
             String mutation = "mutation {\n" +
                     "  createOutputs(input: {JobNumber: \"" + jobNumber + "\"}) {\n" +
@@ -191,6 +222,20 @@ public class SendRequest {
             //System.out.println(results);
             //results.write(out);
             out.write(results.toString(4));
+        }
+        catch (IOException ignored) {
+
+        }
+    }
+
+    public static void outputJSON() {
+        JSONObject jo = readJSON();
+        String path2 = "C:/Users/rebal/Documents/Pipeline/pipeline2.txt";
+        try (PrintWriter out = new PrintWriter(new FileWriter(path2))){
+            JSONObject results = whenSendPostRequest_thenCorrect(jo);
+            //System.out.println(results);
+            //results.write(out);
+            out.write(jo.toString(4));
         }
         catch (IOException ignored) {
 
